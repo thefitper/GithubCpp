55d699defc504ceb3207c5f21ccc8ae23158e0a1
PiWo <75988328+thefitper@users.noreply.github.com>
2025-11-08 19:56:51 +0700

    Update greeting message in GitHub.cpp

--- GitHub.cpp
+++ GitHub.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello Github Cpp!\n";
+    std::cout << "Hello Github Cpp! Edit from Github web\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
